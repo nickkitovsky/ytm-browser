cc4dd7db85db1625c0358efd91a10675af0b33ff
Nick Kitovsky <1825545+nickkitovsky@users.noreply.github.com>
2024-09-09 00:32:03 +0700

    show playlist and tracks is working not

--- dev_app.py
+++ dev_app.py
@@ -4,5 +4,5 @@ from ytm_browser import start_endpoints
 from ytm_browser.textual_ui.browse_app import DevApp
 
 if __name__ == "__main__":
-    app = DevApp()
+    app = DevApp(start_endpoints.endpoints)
     app.run()
